899c412550ceca184feec9c5cb868828ce1f45e4
meteorshowering <3439196372@qq.com>
2024-11-10 16:01:39 +0800

    add pred

--- popularity_prepro.py
+++ popularity_prepro.py
@@ -46,7 +46,7 @@ plt.show()
 #             csv_writer.writerow([key,df.loc[i]['年份'],df.loc[i]['户籍人口（万人）']])
           
 # 插值
-year=[int(i) for i in range(2000,2023)]
+year=[int(i) for i in range(2000,2024)]
 for key in cities:
     if ('live_popularity' not in cities[key]):
         print(key)
@@ -93,49 +93,56 @@ with open("./popularity/live_popularity_process.csv","w+",encoding="utf-8",newli
         for i in range(1,len(df)-1):
             csv_writer.writerow([key,df.loc[i]['年份'],df.loc[i]['常住人口（万人）']])
 
-
-for key in cities:
-    if ('born_popularity' not in cities[key]):
-        print(key)
-        continue
-    df = cities[key]['born_popularity']
-    df = df.dropna(axis=0,how='any')
-    data_train = df.iloc[[int(i) for i in range(0, int(0.8 * len(df)))]]
-    data_train_x = data_train[['年份']]
-    data_train_y2 = data_train['户籍人口（万人）']
-    clf2 = LinearRegression()
-    clf2.fit(data_train_x, data_train_y2)
-    for i in year:
-        if(len(df[df['年份'] == i])==0):
-            yred = clf2.predict([[i]])
-            cities[key]['born_popularity'] = cities[key]['born_popularity']._append(pd.DataFrame([[i, yred[0]]], columns=df.columns))
-        elif(len(df[df['年份'] == i])==1):
-            pass
-        else:
-            mean_of_i=df[df['年份'] == i].mean()
-            cities[key]['born_popularity']=cities[key]['born_popularity'].drop(df[df['年份'] == i].index)
-            cities[key]['born_popularity'] = cities[key]['born_popularity']._append(
-                pd.DataFrame([[i, mean_of_i['户籍人口（万人）']]], columns=df.columns))
-            
-            cities[key]['born_popularity'] = cities[key]['born_popularity'].sort_values(by=['年份'],ascending=[True])
-    cities[key]['born_popularity'].reset_index(drop=True)
-    # print(cities[key]['popularity density'])
-    dff = cities[key]['born_popularity']
-    #print(dff)
-    plt.plot(dff.iloc[:,0],dff.iloc[:,1],label=key)
-plt.title('born_popularity after pre process.')
-plt.xlabel('year')
-plt.ylabel('born_popularity')
-plt.legend(title='city')
-plt.grid(True)
-plt.savefig('./popularity/born_popularity_process.jpg')
-plt.show()
-with open("./popularity/born_popularity_process.csv","w+",encoding="utf-8",newline="") as f:
+with open("./popularity/live_popularity_2023.csv","w+",encoding="utf-8",newline="") as f:
     csv_writer=csv.writer(f)
-    csv_writer.writerow(["city","year","born_popularity"])
+    csv_writer.writerow(["city_id","year","pred"])
     for key in cities:
-        df=cities[key]['born_popularity']
+        df = cities[key]['live_popularity']
         df.reset_index(drop=True,inplace=True)
         print(df)
-        for i in range(1,len(df)-1):
-            csv_writer.writerow([key,df.loc[i]['年份'],df.loc[i]['户籍人口（万人）']])
+        csv_writer.writerow([key,2023,df.loc[23,'年份']])
+# for key in cities:
+#     if ('born_popularity' not in cities[key]):
+#         print(key)
+#         continue
+#     df = cities[key]['born_popularity']
+#     df = df.dropna(axis=0,how='any')
+#     data_train = df.iloc[[int(i) for i in range(0, int(0.8 * len(df)))]]
+#     data_train_x = data_train[['年份']]
+#     data_train_y2 = data_train['户籍人口（万人）']
+#     clf2 = LinearRegression()
+#     clf2.fit(data_train_x, data_train_y2)
+#     for i in year:
+#         if(len(df[df['年份'] == i])==0):
+#             yred = clf2.predict([[i]])
+#             cities[key]['born_popularity'] = cities[key]['born_popularity']._append(pd.DataFrame([[i, yred[0]]], columns=df.columns))
+#         elif(len(df[df['年份'] == i])==1):
+#             pass
+#         else:
+#             mean_of_i=df[df['年份'] == i].mean()
+#             cities[key]['born_popularity']=cities[key]['born_popularity'].drop(df[df['年份'] == i].index)
+#             cities[key]['born_popularity'] = cities[key]['born_popularity']._append(
+#                 pd.DataFrame([[i, mean_of_i['户籍人口（万人）']]], columns=df.columns))
+            
+#             cities[key]['born_popularity'] = cities[key]['born_popularity'].sort_values(by=['年份'],ascending=[True])
+#     cities[key]['born_popularity'].reset_index(drop=True)
+#     # print(cities[key]['popularity density'])
+#     dff = cities[key]['born_popularity']
+#     #print(dff)
+#     plt.plot(dff.iloc[:,0],dff.iloc[:,1],label=key)
+# plt.title('born_popularity after pre process.')
+# plt.xlabel('year')
+# plt.ylabel('born_popularity')
+# plt.legend(title='city')
+# plt.grid(True)
+# plt.savefig('./popularity/born_popularity_process.jpg')
+# plt.show()
+# with open("./popularity/born_popularity_process.csv","w+",encoding="utf-8",newline="") as f:
+#     csv_writer=csv.writer(f)
+#     csv_writer.writerow(["city","year","born_popularity"])
+#     for key in cities:
+#         df=cities[key]['born_popularity']
+#         df.reset_index(drop=True,inplace=True)
+#         print(df)
+#         for i in range(1,len(df)-1):
+#             csv_writer.writerow([key,df.loc[i]['年份'],df.loc[i]['户籍人口（万人）']])
